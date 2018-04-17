ec21fd1e0e841c28a8ac8490a710c0bb07bd2ad6
jwchen119 <jwchen119@gmail.com>
2018-04-17 22:39:48 +0800

    Make it more detailed on 2-6

--- Chapter2/2-6.py
+++ Chapter2/2-6.py
@@ -3,5 +3,8 @@ minute = 1
 second_per_hour = 60 * second * 60 * minute
 seconds_per_day = second_per_hour * 24
 
+print('浮點除法:')
+print(seconds_per_day / second_per_hour)
+print('整數除法:')
 print(seconds_per_day // second_per_hour)
-print('本次結果第5題結果一致')
+print('除法除浮點特性外結果一致')
